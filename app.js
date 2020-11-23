099ef2edae685be996b3d4d06d16d9aeb3c2cd03
Seemant Sinha <sinhaseemant@gmail.com>
2020-11-23 15:10:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res, next) => {
+    res.sendFile(__dirname + "/form.html");
+  });
+
+app.post('/quiz', (req, res, next) => {
+     const answer = "NodeJs Framework";
+    const chosen = req.body.option;
+
+    if (chosen == answer) {
+        res.send("correct answer");
+
+    } else {
+        res.send("wrong answer");
+    }
+    
+});
+app.listen(3000);
